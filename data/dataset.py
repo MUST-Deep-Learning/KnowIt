7348059c45165fa257748653f021d9238de2c629
Tian <tiantheunissen@gmail.com>
2023-08-25 15:49:54 +0200

    Creates prepared data subclass from base dataset class

--- data/dataset.py
+++ data/dataset.py
@@ -13,62 +13,76 @@ from helpers.read_configs import safe_dump
 logger = get_logger()
 
 
-class Dataset:
+class BaseDataset:
 
-    def __init__(self, name: str,
-                 task: str,
-                 input_components: list,
-                 target_components: list,
-                 time_delta: pd.Timedelta,
-                 io_window: tuple,
-                 the_data: dict):
+    def __init__(self, value, init_method: str):
 
-        self.name = name
-        self.task = task
-        self.input_components = input_components
-        self.target_components = target_components
-        self.time_delta = time_delta
-        self.io_window = io_window
-        self.the_data = the_data
+        self.name = ''
+        self.task = ''
+        self.input_components = []
+        self.target_components = []
+        self.time_delta = None
+        self.io_window = None
+        self.the_data = None
 
-        self.instances = list(the_data.keys())
-        self.num_instances = len(self.instances)
-        self.num_slices = sum([len(the_data[i]) for i in the_data])
-        self.num_timesteps = sum([sum([len(s['t']) for s in the_data[i]]) for i in the_data])
-        self.num_target_timesteps = sum([sum([np.count_nonzero(~np.isnan(s['y']).any(axis=1))
-                                                         for s in the_data[i]])
-                                                    for i in the_data])
+        self.instances = []
+        self.num_instances = None
+        self.num_slices = None
+        self.num_timesteps = None
+        self.num_target_timesteps = None
 
+        if init_method == 'option':
+            self.args_from_option(value)
+        elif init_method == 'path':
+            self.args_from_path(value)
+        elif init_method == 'df':
+            self.args_from_df(value)
+        else:
+            self.populate_meta(value)
 
-
-    @classmethod
-    def from_dict(cls, args_dict: str):
-        """create class from dictionary """
-        return cls(**args_dict)
-
-    @classmethod
-    def from_path(cls, path: str, safe_mode=True):
-        """create class from path to raw data """
+    def args_from_path(self, path: str, safe_mode=True):
+        """create meta from path to raw data """
         args_dict = convert_df_from_path(path, fill_nans=True)
         safe_dump(args_dict, dataset_path(args_dict['name']), safe_mode)
-        return cls(**args_dict)
+        self.populate_meta(args_dict)
 
-    @classmethod
-    def from_df(cls, df: pd.DataFrame, safe_mode=True):
-        """create class from dataframe """
+    def args_from_df(self, df: pd.DataFrame, safe_mode=True):
+        """create meta from dataframe """
         args_dict = convert_df(df, fill_nans=True)
         safe_dump(args_dict, dataset_path(args_dict['name']), safe_mode)
-        return cls(**args_dict)
+        self.populate_meta(args_dict)
 
-    @classmethod
-    def from_option(cls, option: str):
-        """create class from option """
+    def args_from_option(self, option: str):
+        """create meta from option """
         try:
             args_dict = load_from_path(dataset_path(option))
         except:
             exit(101)
-        return cls(**args_dict)
+        self.populate_meta(args_dict)
+
+    def populate_meta(self, args):
+
+        self.name = args['name']
+        self.task = args['task']
+        self.input_components = args['input_components']
+        self.target_components = args['target_components']
+        self.time_delta = args['time_delta']
+        self.io_window = args['io_window']
+        self.the_data = args['the_data']
+
+        self.instances = list(self.the_data.keys())
+        self.num_instances = len(self.instances)
+        self.num_slices = sum([len(self.the_data[i]) for i in self.the_data])
+        self.num_timesteps = sum([sum([len(s['t']) for s in self.the_data[i]]) for i in self.the_data])
+        self.num_target_timesteps = sum([sum([np.count_nonzero(~np.isnan(s['y']).any(axis=1))
+                                              for s in self.the_data[i]])
+                                         for i in self.the_data])
 
 
 # DS = Dataset.from_path('/home/tian/postdoc_work/knowit/dummy_raw_data/dummy_zero/dummy_zero.pickle')
-# DS = Dataset.from_option('dummy_zero')
+# DS = BaseDataset.from_option('dummy_zero')
+# ping = 0
+
+# DS = BaseDataset('/home/tian/postdoc_work/knowit/dummy_raw_data/dummy_zero/dummy_zero.pickle', init_method='path')
+# DS = BaseDataset('dummy_zero', init_method='option')
+# ping = 0
--- data/ki_data.py
+++ data/ki_data.py
@@ -1,2 +1,33 @@
 __author__ = 'tiantheunissen@gmail.com'
-__description__ = 'Contains the ki_data module.'
+__description__ = 'Contains the ki_data module (a.k.a prepared data).'
+
+from data.dataset import BaseDataset
+
+class KIDataset(BaseDataset):
+
+    def __init__(self, name: str,
+                 split_method: str,
+                 scaling_method: str,
+                 sampling_method: str,
+                 min_chunk: int,
+                 augment_method: str,
+                 dim_reduction_method: str):
+
+        super().__init__(name, 'option')
+
+        self.split_method = split_method
+        self.scaling_method = scaling_method
+        self.sampling_method = sampling_method
+        self.min_chunk = min_chunk
+        self.augment_method = augment_method
+        self.dim_reduction_method = dim_reduction_method
+
+        ping = 0
+
+
+DS = KIDataset(name='dummy_zero', split_method='chrono',
+               scaling_method='z', sampling_method='random',
+               min_chunk=10, augment_method='None',
+               dim_reduction_method='None')
+ping = 0
+
