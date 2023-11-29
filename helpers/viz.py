e4ca4c7139af24b6e6896d2803f7b2ab99621c73
Tian <tiantheunissen@gmail.com>
2023-11-29 09:13:05 +0200

    Added a set_prediction visual for classification tasks.

--- helpers/viz.py
+++ helpers/viz.py
@@ -7,7 +7,9 @@ from collections import defaultdict
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import timedelta
+from datetime import timedelta, datetime
+import pytz
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 from helpers.logger import get_logger
 
@@ -20,6 +22,7 @@ back_color = 'black'
 grid_color = 'dimgray'
 generic_figsize = (10, 6)
 generic_dpi = 200
+generic_cmap = 'plasma'
 c_cycle = [train_color, valid_color, eval_color, 'darkorange', 'mediumblue']
 matplotlib.rcParams['axes.prop_cycle'] = matplotlib.cycler(color=c_cycle)
 
@@ -67,21 +70,32 @@ def set_predictions(id_args, data_tag):
         y = np.array([i_targets[x][1] for x in order])
         y_hat = np.array([i_predictions[x][1] for x in order])
 
-        # insert NaNs in gaps
         deltas = np.diff(t)
         delta = deltas.min()
-        a_nice_nan = np.empty_like(y[0])
-        a_nice_nan[:] = np.nan
-        while (deltas != delta).any():
-            logger.error('THIS NEEDS CHECKING ASAP!')
-            exit(101)
+        if (deltas != delta).any():
+            # insert NaNs in gaps
+            a_nice_nan = np.empty_like(y[0])
+            a_nice_nan[:] = np.nan
             here = np.argwhere(deltas != delta)
-            for h in range(len(here), 0, -1):
-                t = np.insert(t, h, t[h] + delta)
-                y = np.insert(y, h, a_nice_nan, axis=0)
-                y_hat = np.insert(y_hat, h, a_nice_nan, axis=0)
+            gaps = []
+            for h in range(len(here)):
+                to_insert_nan = here[h].item()
+                gap = np.arange(start=t[to_insert_nan] + delta,
+                                stop=t[to_insert_nan + 1],
+                                step=delta).astype(datetime)
+                gap = np.array([pytz.utc.localize(g) for g in gap])
+                gaps.append(gap)
+            for g in range(len(gaps)-1, 0, -1):
+                to_insert_nan = here[g].item() + 1
+                t = np.insert(t, to_insert_nan, gaps[g])
+                nan_stack = np.stack([a_nice_nan for x in range(len(gaps[g]))], axis=0)
+                y = np.insert(y, to_insert_nan, nan_stack, axis=0)
+                y_hat = np.insert(y_hat, to_insert_nan, nan_stack, axis=0)
             deltas = np.diff(t)
-            delta = deltas.min()
+            if delta != deltas.min():
+                logger.error('Something went very wrong with growing gaps in prediction vizuals.')
+                exit(101)
+
         predictions[i] = [t, y_hat]
         targets[i] = [t, y]
 
@@ -96,35 +110,93 @@ def set_predictions(id_args, data_tag):
 
     for i in instances:
 
-        fig, ax = plt.subplots(1, 1, figsize=generic_figsize)
-        x = predictions[i][0]
-        y_hat = predictions[i][1]
-        y = targets[i][1]
+        if model_args['data']['task'] == 'regression':
+            regression_set_prediction(i, predictions, targets, data_tag,
+                                      out_components, predictions_dir)
+        elif model_args['data']['task'] == 'classification':
+            classification_set_prediction(i, predictions, targets, data_tag,
+                                          out_components, predictions_dir, model_args)
+        else:
+            logger.error('Unknown task type %s.', model_args['data']['task'])
+            exit(101)
 
-        y_time = y_hat.shape[1]
-        y_components = y_hat.shape[2]
 
-        for t in range(y_time):
-            for c in range(y_components):
-                map = ax.plot(x, y[:, t, c],
-                              label='Target ' + out_components[c] + ' step ' + str(t))
-                mae = np.mean(np.abs(y[:, t, c] - y_hat[:, t, c]))
-                ax.plot(x, y_hat[:, t, c],
-                        label='Predicted ' + out_components[c] + ' step ' + str(t) + ' (mae=' + str(mae) + ')',
-                        linestyle='--', color=map[-1].get_color())
-        ax.set_title(data_tag + ' Instance: ' + str(i))
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Feature value')
-        ax.set_facecolor(back_color)
-        ax.grid(color=grid_color, alpha=0.5)
-        plt.legend()
+def regression_set_prediction(i, predictions, targets, data_tag, out_components, predictions_dir):
 
-        save_path = os.path.join(predictions_dir, data_tag + '-prediction-' + str(i) + '.png')
-        plt.savefig(save_path, dpi=generic_dpi)
+    fig, ax = plt.subplots(1, 1, figsize=generic_figsize)
+    x = predictions[i][0]
+    y_hat = predictions[i][1]
+    y = targets[i][1]
 
-        # plt.show()
-        plt.close()
+    y_time = y_hat.shape[1]
+    y_components = y_hat.shape[2]
 
+    for t in range(y_time):
+        for c in range(y_components):
+            map = ax.plot(x, y[:, t, c],
+                          label='Target ' + out_components[c] + ' step ' + str(t))
+            mae = np.mean(np.abs(y[:, t, c] - y_hat[:, t, c]))
+            ax.plot(x, y_hat[:, t, c],
+                    label='Predicted ' + out_components[c] + ' step ' + str(t) + ' (mae=' + str(mae) + ')',
+                    linestyle='--', color=map[-1].get_color())
+    ax.set_title(data_tag + ' Instance: ' + str(i))
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Feature value')
+    ax.set_facecolor(back_color)
+    ax.grid(color=grid_color, alpha=0.5)
+    plt.legend()
+
+    save_path = os.path.join(predictions_dir, data_tag + '-prediction-' + str(i) + '.png')
+    plt.savefig(save_path, dpi=generic_dpi)
+
+
+def classification_set_prediction(i, predictions, targets, data_tag,
+                                  out_components, predictions_dir, model_args):
+
+    def special_nanargmax(arr, axis):
+        new_arr = np.zeros(arr.shape[0]) + np.nan
+        d0 = np.isnan(arr).all(axis=axis)
+        new_arr[~d0] = np.nanargmax(arr[~d0], axis=1)
+        return new_arr
+
+    fig, axes = plt.subplots(1, 2, figsize=generic_figsize)
+    x = predictions[i][0]
+    y_hat = special_nanargmax(predictions[i][1], axis=1)
+    y = special_nanargmax(targets[i][1], axis=1)
+    class_set = model_args['data_dynamics']['class_set']
+    class_labels = [c for c in class_set.keys()]
+    correct = y == y_hat
+    correct = 1. * correct
+    nan_mask = np.isnan(y)
+    correct[nan_mask] = np.nan
+    accuracy = np.count_nonzero(correct[~nan_mask]) / float(len(correct[~nan_mask]))
+    ax = axes[0]
+    ax.plot(x, y, label='Target class')
+    ax.plot(x, y_hat, label='Predicted class')
+    ax.set_xlim(x.min(), x.max())
+    ax.set_title(data_tag + ' Instance: ' + str(i) + ': accuracy ' + str(accuracy))
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Class')
+    ax.set_facecolor(back_color)
+    ax.set_yticks([c for c in range(len(class_labels))])
+    ax.set_yticklabels(class_labels)
+    ax.grid(color=grid_color, alpha=0.5)
+    ax.legend()
+    ax = axes[1]
+    num_classes = len(class_set)
+    conf_mat = np.zeros(shape=(num_classes, num_classes))
+    for c_predicted in range(num_classes):
+        for c_target in range(num_classes):
+            t_hits = y == c_target
+            p_hits = y_hat == c_predicted
+            val = np.count_nonzero(np.logical_and(t_hits, p_hits))
+            conf_mat[c_target, c_predicted] = val
+
+    ConfusionMatrixDisplay(confusion_matrix=conf_mat,
+                           display_labels=[c for c in class_set.keys()]).plot(ax=ax, cmap=generic_cmap)
+    plt.tight_layout()
+    save_path = os.path.join(predictions_dir, data_tag + '-prediction-' + str(i) + '.png')
+    plt.savefig(save_path, dpi=generic_dpi)
 
 def learning_curves(id_args):
 
