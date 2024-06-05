d3a30ef1d650f11f471ddaa7beb5db56106c8a96
Randle Rabe <randlerabe@gmail.com>
2024-06-05 10:21:19 +0200

    Added docstrings, headers, and annotations. Made some parts of the code more precise.

--- default_archs/MLP.py
+++ default_archs/MLP.py
@@ -1,68 +1,108 @@
-__author__ = 'randlerabe@gmail.com, tiantheunissen@gmail.com'
-__description__ = 'This is an MLP.'
+"""
+---------------------
+Multilayer Perceptron
+---------------------
 
-from typing import Union
-import torch.nn as nn
+As an example, we define a multilayer perceptron (MLP) architecture using the
+well-known Pytorch library.
+
+The example shows one way of constructing the MLP which can either be shallow
+or deep. The MLP is capable of handling regression or classification tasks.
+"""  # noqa: INP001, D415, D400, D212, D205
+
+from __future__ import annotations
+
+__author__ = "randlerabe@gmail.com, tiantheunissen@gmail.com"
+__description__ = "This is an MLP."
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from torch import Tensor
+    from torch.nn import Module
+
 import numpy as np
+from torch import nn
 
-available_tasks = ('regression', 'classification')
-
-# Default hyperparameter values
-HP_defaults_dict = {'depth': 3,
-           'width': 256,
-           'batchnorm': True,
-           'dropout': 0.5,
-           'activations': 'ReLU',
-           'output_activation': None}
+available_tasks = ("regression", "classification")
 
 # The ranges for each hyperparameter (used later for Knowit Tuner module)
-HP_ranges_dict = {'depth': range(1, 21, 1),
-           'width': range(2, 1025, 1),
-           'batchnorm': (True, False),
-           'dropout': np.arange(0, 1.1, 0.1),
-           'activations': ('ReLU', 'LeakyReLU', 'Tanh', 'GLU'), # see Pytorch docs for more options
-           'output_activation': (None, 'Sigmoid', 'Softmax')}
+HP_ranges_dict = {
+    "depth": range(1, 21, 1),
+    "width": range(2, 1025, 1),
+    "batchnorm": (True, False),
+    "dropout": np.arange(0, 1.1, 0.1),
+    "activations": (
+        "ReLU",
+        "LeakyReLU",
+        "Tanh",
+        "GLU",
+    ),  # see Pytorch docs for more options
+    "output_activation": (None, "Sigmoid", "Softmax"),
+}
+
 
 class Model(nn.Module):
-    """
-    Model Name
-    Paper link: ???
+    """Define MLP architecture.
+
+    The multilayer perceptron (MLP) is a fully connected feedforward neural
+    network with nonlinear activation functions.
+
+    For more information on this architecture, see for example:
+    [1] "Deep Learning" by I. Goodfellow, Y. Bengio, and A. Courville
+    Link: https://www.deeplearningbook.org/
+
+    [2] "Understanding Deep Learning" by S.J.D Prince
+    Link: https://udlbook.github.io/udlbook/
     """
 
-    def __init__(self, 
-                 input_dim: list,
-                 output_dim: list,
-                 task_name: str, 
-                 depth: int = HP_defaults_dict['depth'], 
-                 width: int = HP_defaults_dict['width'], 
-                 batchnorm: bool = HP_defaults_dict['batchnorm'], 
-                 dropout: float = HP_defaults_dict['dropout'], 
-                 activations: str = HP_defaults_dict['activations'],
-                 output_activation: Union[str, None] = HP_defaults_dict['output_activation']
-                 ):
-        
-        super(Model, self).__init__()
-        
-        # Input data type check
-        if not isinstance(input_dim, list):
-            raise TypeError("input_dim must be of type int")
-        if not isinstance(output_dim, list):
-            raise TypeError("output_dim must be of type int")
-        if not isinstance(task_name, str):
-            raise TypeError("task_name must be of type str")
-        if not isinstance(depth, int):
-            raise TypeError("depth must be of type int")
-        if not isinstance(width, int):
-            raise TypeError("width must be of type int")
-        if not isinstance(batchnorm, bool):
-            raise TypeError("batchnorm must be of type bool")
-        if not isinstance(dropout, float):
-            raise TypeError("dropout must be of type float")
-        if not isinstance(activations, str):
-            raise TypeError("activations must be of type str")
-        if not isinstance(output_activation, str) and output_activation is not None:
-            raise TypeError("output_activation must be of type str or None")
-        
+    def __init__(
+        self,
+        input_dim: list[int],
+        output_dim: list[int],
+        task_name: str,
+        depth: int = 3,
+        width: int = 256,
+        dropout: float = 0.5,
+        activations: str = "ReLU",
+        output_activation: None | str = None,
+        *,
+        batchnorm: bool = True,
+    ) -> None:
+        """Model constructor.
+
+        Args:
+        ----
+            input_dim (list[int]):  The model's input features of shape
+                                    [in_chunk, in_components].
+
+            output_dim (list[int]): The model's output features of shape
+                                    [out_chunk, out_components].
+
+            task_name (str):        The type of task (classification or
+                                    regression).
+
+            depth (int):            The number of hidden layers. Default: 3.
+
+            width (int):            The width of each hidden layer.
+                                    Default: 256.
+
+            dropout (float):
+                                    Sets the dropout value. Default: 0.5.
+
+            activations (str):
+                                    Sets the activation type for the hidden
+                                    units. Defaults: "ReLU".
+
+            output_activation (None | str):
+                                    Sets an output activation (needed for
+                                    classification tasks). Default: None.
+
+            batchnorm (bool):       Adds batchnorm to layers. Default: True.
+
+        """
+        super().__init__()  # type: ignore[reportUnknownMemberType]
+
         # Hyperparameters
         self.task_name = task_name
         self.hidden_depth = depth
@@ -75,82 +115,132 @@ class Model(nn.Module):
         self.model_in_dim = int(np.prod(input_dim))
         self.model_out_dim = int(np.prod(output_dim))
         self.final_out_dim = output_dim
-        
+
         # Input layer
-        layers = []
-        layers.append(nn.Linear(self.model_in_dim, self.hidden_width, bias=True))
+        layers: list[Module] = []
+        layers.append(
+            nn.Linear(
+                self.model_in_dim,
+                self.hidden_width,
+                bias=True,
+            ),
+        )
         if self.batchnorm:
             layers.append(nn.BatchNorm1d(self.hidden_width))
-        layers.append(getattr(nn, self.activations)()) 
+        layers.append(getattr(nn, self.activations)())
         layers.append(nn.Dropout(p=self.dropout))
-        
+
         # Construct MLP hidden layers
         layers = layers + self._build_hidden_layers()
-        
+
         # Set output layer according to task type
-        assert task_name in available_tasks, "Error: Task must be either: \'regression\' or \'classification\'"
+        if task_name not in available_tasks:
+            emsg = "Task must be either: 'regression' or 'classification'."
+            raise NameError(emsg)
         self.task_name = task_name
-        
-        if self.task_name == 'regression':
-            output_layer = nn.Linear(self.hidden_width, self.model_out_dim, bias=False)
+
+        if self.task_name == "regression":
+            output_layer = nn.Linear(
+                self.hidden_width,
+                self.model_out_dim,
+                bias=False,
+            )
             layers.append(output_layer)
-        if self.task_name == 'classification':
-            output_layer = nn.Linear(self.hidden_width, self.model_out_dim, bias=False)
-            if self.output_activation == 'Softmax':
-                output_layer_activation = getattr(nn, self.output_activation)(dim=1)
+        if self.task_name == "classification":
+            output_layer = nn.Linear(
+                self.hidden_width,
+                self.model_out_dim,
+                bias=False,
+            )
+            if self.output_activation == "Softmax":
+                output_layer_activation = getattr(nn,
+                                                  self.output_activation)(dim=1)
             elif self.output_activation is None:
                 output_layer_activation = nn.Identity()
             else:
                 output_layer_activation = getattr(nn, self.output_activation)()
             layers.append(output_layer)
             layers.append(output_layer_activation)
-            
+
         # Merge layers together in Sequential
         self.model = nn.Sequential(*layers)
-            
-    def _build_hidden_layers(self):
-        """Construct the hidden layers of the MLP using the user-specified parameters."""
-        
-        hidden_blocks = []
+
+
+    def _build_hidden_layers(self) -> list[Module]:
+        hidden_blocks: list[Module] = []
         for _ in range(self.hidden_depth - 1):
-            hidden_blocks.append(nn.Linear(self.hidden_width, self.hidden_width, bias=True))
+            hidden_blocks.append(
+                nn.Linear(self.hidden_width, self.hidden_width, bias=True),
+            )
             if self.batchnorm:
                 hidden_blocks.append(nn.BatchNorm1d(self.hidden_width))
-            hidden_blocks.append(getattr(nn, self.activations)()) 
+            hidden_blocks.append(getattr(nn, self.activations)())
             hidden_blocks.append(nn.Dropout(p=self.dropout))
-        
+
         return hidden_blocks
-            
 
-    def regression(self, input):
-        """Performs a forward pass over the regression MLP.
-        
-        Returns a tensor of shape (batch_size, output_dim)"""
 
-        out = self.model(input)
-        out = out.view(out.shape[0], self.final_out_dim[0], self.final_out_dim[1])
-        
-        return out
+    def _regression(self, x: Tensor) -> Tensor:
+        """Return model output for an input batch for a regression task.
 
-    def classification(self, input):
-        """Performs a forward pass over the classifier MLP.
-        
-        Returns a tensor of shape (batch_size, output_dim)"""
+        Args:
+        ----
+            x (Tensor):     An input tensor of shape
+                            (batch_size, in_chunk * in_components)
 
-        out = self.model(input)
-          
-        return out
+        Returns:
+        -------
+            (Tensor):       Model output of shape
+                            (batch_size, out_chunk, out_components)
 
-    def forward(self, input):
-        """Based on the user task, passes the input to the relevant forward function.
-        
-        Returns an output tensor of shape (batch_size, output_dim)"""
+        """
+        out = self.model(x)
 
-        input = input.view(input.shape[0], self.model_in_dim)
-        
-        if self.task_name == 'regression':
-            output = self.regression(input)
-            return output
-        if self.task_name == 'classification':
-            output = self.classification(input)
-            return output
+        return out.view(
+            out.shape[0],
+            self.final_out_dim[0],
+            self.final_out_dim[1],
+        )
+
+
+    def _classification(self, x: Tensor) -> Tensor:
+        """Return model output for an input batch for a classification task.
+
+        Args:
+        ----
+            x (Tensor):     An input tensor of shape
+                            (batch_size, in_chunk * in_components)
+
+        Returns:
+        -------
+            (Tensor):       Model output of shape
+                            (batch_size, num_classes)
+
+        """
+        return self.model(x)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Return model output for an input batch.
+
+        Args:
+        ----
+            x (Tensor):     An input tensor of shape
+                            (batch_size, in_chunk, in_components).
+
+        Returns:
+        -------
+            (Tensor):       Model output of shape
+                            (batch_size, out_chunk, out_components) if regress-
+                            ion.
+
+                            Model output of shape
+                            (batch_size, num_classes) if classification.
+
+        """
+        x = x.view(x.shape[0], self.model_in_dim)
+
+        if self.task_name == "regression":
+            return self._regression(x)
+
+        return self._classification(x)
