c16e9ced72c5a05f620123cd58599a4c4c575b2c
Harmen Potgieter <potgieterharmen@gmail.com>
2026-01-06 11:34:10 +0200

    Added author

--- knowit.py
+++ knowit.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 __copyright__ = 'Copyright (c) 2025 North-West University (NWU), South Africa.'
 __licence__ = 'Apache 2.0; see LICENSE file for details.'
-__author__ = 'tiantheunissen@gmail.com, randlerabe@gmail.com'
+__author__ = 'tiantheunissen@gmail.com, randlerabe@gmail.com, potgieterharmen@gmail.com'
 __description__ = 'Contains the main KnowIt module.'
 
 # external imports
