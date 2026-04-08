0f02ae53fc5d11e3957809f2c0dfdcff414b0e3c
Harmen Potgieter <potgieterharmen@gmail.com>
2026-04-08 07:34:37 +0200

    Improved keyword metadata in `pyproject.toml`, adjusted `safe_mode` behavior in tests, added cleanup step for test output, and introduced `test_advance.py` for advanced functionality coverage.

--- tests/test_advance.py
+++ tests/test_advance.py
@@ -1,47 +1,49 @@
 # Import the KnowIt module
-import shutil
 from knowit import KnowIt
+import shutil
 
 # Instantiate a KnowIt object linked to an output directory
 KI = KnowIt(custom_exp_dir='knowit_temp_test', safe_mode=False)
+model_name = 'Tookit_test_lstm'
 
 # Define your dataset
 data_args = {'name': 'synth_2',
-             'task': 'regression',
+             'task': 'vl_regression',
              'in_components': ['x1', 'x2', 'x3', 'x4'],
              'out_components': ['x5'],
-             'in_chunk': [-32, 0],
-             'out_chunk': [0, 0],
+             'in_chunk': [0, 0],
+             'out_chunk': [2, 2],
              'split_portions': [0.6, 0.2, 0.2],
-             'batch_size': 64}
+             'batch_size': 64,
+             'batch_sampling_mode': 'variable_length',
+             'padding_method': 'constant'}
 
 # Define your architecture
-arch_args = {'task': 'regression',
-             'name': 'MLP'}
+arch_args = {'task': 'vl_regression',
+             'name': 'LSTMv2'}
 
 # Define your trainer
 trainer_args = {'loss_fn': 'mse_loss',
                 'optim': 'Adam',
                 'max_epochs': 1,
                 'learning_rate': 0.01,
-                'task': 'regression'}
+                'task': 'vl_regression'}
 
 # Train your model
-KI.train_model(model_name='my_new_model_name',
+KI.train_model(model_name=model_name,
                kwargs={'data': data_args,
                        'arch': arch_args,
-                       'trainer': trainer_args}, safe_mode=False)
+                       'trainer': trainer_args}, safe_mode=False, and_viz=True)
 
 # Generate model predictions
-KI.generate_predictions(model_name='my_new_model_name',
-                        kwargs={'predictor': {'prediction_set': 'valid'}}, safe_mode=False)
+KI.generate_predictions(model_name=model_name,
+                        kwargs={'predictor': {'prediction_set': 'valid'}}, safe_mode=False, and_viz=True)
 
-# Interpret model predictions
-KI.interpret_model(model_name='my_new_model_name',
+# # Interpret model predictions
+KI.interpret_model(model_name=model_name,
                    kwargs={'interpreter':
                                {'interpretation_method': 'DeepLift',
                                 'interpretation_set': 'valid',
                                 'selection': 'random',
-                                'size': 100}}, safe_mode=False)
-
+                                'size': 100}}, and_viz=True, safe_mode=False)
 shutil.rmtree(KI.exp_output_dir)
